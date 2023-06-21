c2de7b160f0f56d3bc40e4ec87e83d0062a5f484
yipianfan <secretener@gmail.com>
2023-06-21 08:40:02 +0800

    add TimeServerHandler.java

---
+++ src/main/java/com/simple/netty/TimeServerHandler.java
@@ -0,0 +1,33 @@
+package com.simple.netty;
+
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+
+public class TimeServerHandler implements Runnable {
+    private Socket socket;
+
+    public TimeServerHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+        try (LineNumberReader in = new LineNumberReader(new InputStreamReader(socket.getInputStream()));
+             PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true)) {// 这里必须为true,否则客户端收不到消息
+
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String line = in.readLine();
+            while (!"EOF".equals(line)) {// 用 null != line 判断会有socket重置异常
+                System.out.println("Receive order is: " + line);
+                String curTime = "QUERY TIME ORDER".equals(line) ? sdf.format(System.currentTimeMillis()) : "BAD ORDER";
+                pw.println(curTime); // 返回给客户端的数据
+                line = in.readLine();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/simple/netty/TimeServerHandlerV2.java
@@ -0,0 +1,49 @@
+package com.simple.netty;
+
+import java.io.*;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+
+/**
+ * TimeServerHandler的v2版本,当读取客户端流时,用null判断,手动捕获异常.
+ */
+public class TimeServerHandlerV2 implements Runnable {
+    private final Socket socket;
+
+    public TimeServerHandlerV2(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+        BufferedReader in = null;
+        PrintWriter pw = null;
+        try {
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+            String line = in.readLine();
+            while (line != null) { // 用这种方式判断,会抛出socket异常
+                String curTime = "QUERY TIME ORDER".equals(line) ? sdf.format(System.currentTimeMillis()) : "BAD ORDER";
+                pw.println(curTime);
+                line = in.readLine();
+            }
+        } catch (Exception e) { // 为什么不用finally块来关闭流,因为上面的while语句一定会抛出socket异常
+            try {
+                socket.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+            if (pw != null) {
+                pw.close();
+            }
+        }
+    }
+}
