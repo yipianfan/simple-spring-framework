6978b728d1c8874150be90a0ab8d7a20b2a5bad5
yipianfan <secretener@gmail.com>
2023-07-13 15:48:19 +0800

    线程学习

--- src/main/java/com/simple/thread/ThreadDemo.java
+++ src/main/java/com/simple/thread/ThreadDemo.java
@@ -2,7 +2,10 @@ package com.simple.thread;
 
 public class ThreadDemo {
 
-    public static void main(String[] args) throws InterruptedException {
+    /**
+     * 线程按启动顺序执行方案,join方法的原理本质上是调用wait方法.
+     */
+    void demo1() throws InterruptedException {
         Thread t1 = new Thread(() -> System.out.println("#1"));
         Thread t2 = new Thread(() -> System.out.println("#2"));
         Thread t3 = new Thread(() -> System.out.println("#3"));
@@ -23,5 +26,37 @@ public class ThreadDemo {
         t5.start();
         t5.join();
         t6.start();
+    }
+
+    /**
+     * Thread.sleep只会让线程等待,不会让出锁,也不用在同步方法或同步块中调用.
+     */
+    synchronized void demo2() {
+        System.out.println("demo2() called. Thread name is: " + Thread.currentThread().getName());
+        try {
+            Thread.sleep(1000 * 3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("线程结束睡眠");
+    }
+
+    /**
+     * wait、notify 方法一定要在同步方法或同步块中调用.
+     * 调用wait方法期间,线程会释放持有的锁.
+     */
+    synchronized void demo3() {
+        System.out.println("demo3() called. Thread name is: " + Thread.currentThread().getName());
+        try {
+            wait(2000);
+            for (int i = 0; i < 5; i++) {
+                System.out.println(i + " -> " + Thread.currentThread().getName());
+                Thread.sleep(1000);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + "线程结束");
     }
 }
