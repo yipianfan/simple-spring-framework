cdb3f5d4a49f9f3604fb49cb0f5c2d4359c313fc
yipianfan <secretener@gmail.com>
2023-08-15 14:50:06 +0800

    二分查找算法的实现

---
+++ src/main/java/com/simple/algorithm/search/BinarySearch.java
@@ -0,0 +1,24 @@
+package com.simple.algorithm.search;
+
+public class BinarySearch {
+
+    /**
+     * 二分查找,输入的数组必须是一个已经排序的数组.
+     *
+     * @return 目标数字的索引, 找不到则返回-1
+     */
+    public static int search(int[] numbers, int target) {
+        int start = 0, end = numbers.length - 1;
+
+        while (start <= end) {
+            int middle = (start + end) >> 1;
+            if (target == numbers[middle])
+                return middle;
+            else if (target >= numbers[middle])
+                start = middle + 1;
+            else
+                end = middle - 1;
+        }
+        return -1;
+    }
+}
