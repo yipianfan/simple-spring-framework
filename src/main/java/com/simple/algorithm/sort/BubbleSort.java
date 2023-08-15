a15c562e5442c066954144614ed79707b95cd939
yipianfan <secretener@gmail.com>
2023-08-15 17:45:50 +0800

    冒泡排序

--- src/main/java/com/simple/algorithm/sort/BubbleSort.java
+++ src/main/java/com/simple/algorithm/sort/BubbleSort.java
@@ -7,7 +7,7 @@ public class BubbleSort {
 
     public static int[] sort(int[] numbers) {
         for (int i = 0; i < numbers.length; i++) {
-            for (int j = i; j < numbers.length - 1; j++) {
+            for (int j = 0; j < numbers.length - i - 1; j++) {
                 if (numbers[j] > numbers[j + 1]) {
                     int a = numbers[j];
                     numbers[j] = numbers[j + 1];
