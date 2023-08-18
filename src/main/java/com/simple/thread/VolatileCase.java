521c3356e603a7d19356bb84e946f8b7a373e3fd
yipianfan <secretener@gmail.com>
2023-08-18 17:45:48 +0800

    Volatile关键字总结

--- src/main/java/com/simple/thread/VolatileCase.java
+++ src/main/java/com/simple/thread/VolatileCase.java
@@ -1,14 +1,26 @@
 package com.simple.thread;
 
+/**
+ * volatile关键字有两个作用: 1.保证变量的可见性  2.禁止指令的重排性
+ */
 public class VolatileCase {
-    // instance变量必须为volatile,确保instance的可见性,即每次修改instance变量后,立刻更新到主存,每次读数据也是从主存中读
+    // instance变量必须为volatile,确保不会发性指令重排
     private static VolatileCase instance;
     private static boolean isStop;
 
     private VolatileCase() {
     }
 
+    /**
+     * 禁止指令重排案例
+     * instance = new VolatileCase() 语句大概包含了以下3条指令
+     * 1.为对象分配内存空间
+     * 2.初始化对象,即为对象填充数据
+     * 3.将对象内存地址指向到instance变量
+     */
     public static VolatileCase getInstance() {
+        /*为什么instance变量要为volatile,原因就是在这里,如果指令发生重排,得到的instance将是一个没有初始化的对象,
+          所以双重检测造成的问题,是第一个 "instance == null"语句造成的,而不是第二个"instance == null"语句造成的*/
         if (instance == null) {
             synchronized (VolatileCase.class) {
                 if (instance == null) {
@@ -20,9 +32,8 @@ public class VolatileCase {
     }
 
     /**
+     * 可见性案例
      * Thread 1 将永远不会终止,因为isStop不是volatile
-     *
-     * @throws InterruptedException
      */
     public static void test() throws InterruptedException {
         new Thread(() -> {
